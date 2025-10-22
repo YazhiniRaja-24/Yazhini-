0adbd349c91bb889c62b985b8fb602022f585034
YazhiniRaja-24 <yaluraji24@gmail.com>
2025-10-22 11:56:43 +0530

    ADDED

--- project/src/pages/Home.jsx
+++ project/src/pages/Home.jsx
@@ -1,7 +1,13 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const Home = () => {
-  return <h2>Welcome to the Home Page</h2>
+  return (
+    <>
+      <h2>Welcome to the Home Page</h2>
+      <Link to="/state">Go to State Page</Link>
+    </>
+  )
 }
 
 export default Home
